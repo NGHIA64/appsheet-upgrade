42b410059d9316ea6c121216a4807cedf768428c
nghia64 <nguyentrongn168@gmail.com>
2023-03-13 08:18:34 +0700

    add trick

--- test/TestAppsheet/main.js
+++ test/TestAppsheet/main.js
@@ -1,8 +1,16 @@
 //code expand all data table 
 
 const titleElements = document.querySelectorAll('.Title');
-
 titleElements.forEach((element) => {
-    const ariaLabelValue = element.getAttribute('aria-label');
     element.click()
 });
+
+
+
+//Scroll đến action muốn
+
+// Tìm phần tử có thuộc tính data-path tương ứng
+const element = document.querySelector('[data-path="Behavior,Actions,tonKho,Add"]');
+
+// Di chuyển màn hình đến vị trí của phần tử đó
+element.scrollIntoView();
---
+++ test/TestAppsheet/readme.md
@@ -0,0 +1,19 @@
+# Convention Data-path Appsheet
+
+## Data
+
+- Example: Data,Tables,chiTietPhieuXuatKho
+
+## View
+
+- Example: UX,Views,doiTuongChucNang
+
+## Action
+
+- Example: Behavior,Actions,tonKho,Add
+
+## Automation
+
+- Example: Automation,Bots,New bot
+
+# Convention Create Action Name Appsheet
